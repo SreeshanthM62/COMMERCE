ffee6c84e142616b0b0eb11dbdc6650d0a814b91
SreeshanthM62 <sreeshanth0526@gmail.com>
2026-04-26 10:46:08 +0530

    alert commit 3

--- front-end/src/components/Alert.jsx
+++ front-end/src/components/Alert.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alert = () => {
   return (
-    <div>
+    <div className='sticky top-0 z-50'>
       <h1 className='text-red-700 text-center font-extrabold'>!!!!!! THIS WEBSITE IS UNDER CONSTRUCTION, DO NOT ORDER THROUGH WEBSITE, ORDER THROUGH INSTAGRAM FOR NOW !!!!!!</h1>
     </div>
   )
