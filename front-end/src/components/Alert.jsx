3bc0bf5dc7c9ac2c90f1c152d0ff5897aff95313
SreeshanthM62 <sreeshanth0526@gmail.com>
2026-04-26 10:40:12 +0530

    ALERT COMMIT

---
+++ front-end/src/components/Alert.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alert = () => {
+  return (
+    <div>
+      <h1 className='color-red'>!!!!!! THIS WEBSITE IS UNDER CONSTRUCTION, DO NOT ORDER THROUGH WEBSITE, ORDER THROUGH INSTAGRAM FOR NOW !!!!!!</h1>
+    </div>
+  )
+}
+
+export default Alert
