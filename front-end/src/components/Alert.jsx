b79c73164f6b4123a2015e782e7b9e66708bbe23
SreeshanthM62 <sreeshanth0526@gmail.com>
2026-06-03 12:40:34 +0530

    alert new commit 1

--- front-end/src/components/Alert.jsx
+++ front-end/src/components/Alert.jsx
@@ -4,7 +4,7 @@ const Alert = () => {
   return (
     <div className='w-full bg-pink-100 text-pink-700 text-xs sm:text-sm font-medium py-2 px-4 text-center border-b border-pink-200 sticky top-0 z-50 transition-all'>
      <p>
-      The Website is coming soon✨. So, for now please order through
+      The Website is coming soon✨. So, for now please order through 
       <a
         href = 'https://ig.me/m/_twistnbloom.co'
         target = '_blank'
