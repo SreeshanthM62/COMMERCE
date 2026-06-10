fe6a1d94c3b6099cbd1c2fdecf5c4bb8160da325
SreeshanthM62 <sreeshanth0526@gmail.com>
2026-06-10 11:35:11 +0530

    opt_image product item commit

--- front-end/src/components/ProductItem.jsx
+++ front-end/src/components/ProductItem.jsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom"
 
 const ProductItem = ({ id, image, name, price }) => {
 
+    const opt_image = image.replace("/upload/", "/upload/f_auto,q_auto,w_500/")
+
     const { currency,addToCart } = useContext(ShopContext)
     return (
     <div className='flex flex-col justify-between'>
       <Link className="cursor-pointer" to={`/product/${id}`}>
         <div className='overflow-hidden'>
-            <img className='rounded hover:scale-110 transition ease-in-out w-[250px] h-[250px] sm:h-[450px] sm:w-[400px]' src = {image}/>
+            <img className='rounded hover:scale-110 transition ease-in-out w-[250px] h-[250px] sm:h-[450px] sm:w-[400px]' src = {opt_image}/>
         </div>
         <h1 className='pt-3 pb-1 text-sm font-bold font-[BioRhyme] sm:text-[16px]'>{name}</h1>
         <p className='text-sm font-semibold sm:text-[15px]'>{currency}{price}</p>
