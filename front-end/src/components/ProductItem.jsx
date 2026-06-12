3d59e777295da70580ebda8e70411e7cfa1e279f
SreeshanthM62 <sreeshanth0526@gmail.com>
2026-06-12 23:59:08 +0530

    font changes commit product item

--- front-end/src/components/ProductItem.jsx
+++ front-end/src/components/ProductItem.jsx
@@ -13,11 +13,11 @@ const ProductItem = ({ id, image, name, price }) => {
         <div className='overflow-hidden'>
             <img className='rounded hover:scale-110 transition ease-in-out aspect-4/5 sm:aspect-4/5' src = {opt_image}/>
         </div>
-        <h1 className='pt-3 pb-1 text-sm font-bold font-[BioRhyme] sm:text-[16px]'>{name}</h1>
-        <p className='text-sm font-semibold sm:text-[15px]'>{currency}{price}</p>
+        <h1 className='pt-3 pb-1 text-sm font-bold font-[Eczar] sm:text-[16px]'>{name}</h1>
+        <p className='pb-1.5 text-sm font-semibold sm:text-[15px]'>{currency}{price}</p>
       </Link>
 
-        <button onClick={()=>addToCart(id)} className="px-[2px] py-[2px] font-semibold bg-[#FF69B4] border-0 font-[Neuton] cursor-pointer sm:text-[18px]">Add To Cart ❀</button>
+        <button onClick={()=>addToCart(id)} className="py-1.25 font-[Alegreya] font-semibold bg-[#FF69B4] text-white border-0 cursor-pointer sm:text-[18px] rounded-xl">Add To Cart ❀</button>
 
     </div>
 
