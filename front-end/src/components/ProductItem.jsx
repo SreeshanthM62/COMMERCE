bdc0ca59eaf8e0e102323bf6dc99e1e9724052f8
SreeshanthM62 <sreeshanth0526@gmail.com>
2026-06-13 21:23:05 +0530

    image transition commit

--- front-end/src/components/ProductItem.jsx
+++ front-end/src/components/ProductItem.jsx
@@ -4,23 +4,29 @@ import { Link } from "react-router-dom"
 
 const ProductItem = ({ id, image, name, price }) => {
 
-    const { currency,addToCart} = useContext(ShopContext)
-    return (
+  const { currency, addToCart } = useContext(ShopContext)
+  return (
     <div className='flex flex-col justify-between'>
       <Link className="cursor-pointer" to={`/product/${id}`}>
         <div className='overflow-hidden'>
-            <img className='rounded hover:scale-110 transition ease-in-out aspect-4/5 sm:aspect-4/5' src = {image} loading="lazy"/>
+          <div className="overflow-hidden rounded">
+            <img
+              className='aspect-4/5 transition-transform duration-300 ease-in-out hover:scale-110 active:scale-110'
+              src={image}
+              loading="lazy"
+            />
+          </div>
         </div>
         <h1 className='pt-3 pb-1 text-sm font-bold font-[Eczar] sm:text-[16px]'>{name}</h1>
         <p className='pb-1.5 text-sm font-semibold sm:text-[15px]'>{currency}{price}</p>
       </Link>
 
-        <button onClick={()=>addToCart(id)} className="py-1.25 font-[Alegreya] font-semibold bg-[#FF69B4] text-white border-0 cursor-pointer sm:text-[18px] rounded-xl">Add To Cart ❀</button>
+      <button onClick={() => addToCart(id)} className="py-1.25 font-[Alegreya] font-semibold bg-[#FF69B4] text-white border-0 cursor-pointer sm:text-[18px] rounded-xl">Add To Cart ❀</button>
 
     </div>
 
 
-    )
+  )
 }
 
 export default ProductItem
